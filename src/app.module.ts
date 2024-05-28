03825e26071b3f72276f5ef4cc08f0d454eb7b45
k1ju <gksrlwn98@naver.com>
2024-05-28 15:57:22 +0900

    feat: Add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CrimeCityModule } from './crime-city/crime-city.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    CrimeCityModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('PSQL_HOST'),
+        port: parseInt(configService.get<string>('PSQL_PORT')),
+        password: configService.get<string>('PSQL_PW'),
+        username: configService.get<string>('PSQL_USER'),
+        database: configService.get<string>('PSQL_DATABASE'),
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
